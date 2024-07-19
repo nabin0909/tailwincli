57f9e9e20c99c22efa0502e382ce9db436736dbd
unknown <nabindangol311@gmail.com>
2024-07-19 17:06:01 +0545

    tailwindcli

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        nabin:'#6A9C42',
+        om:"red",
+      }
+    },
+  },
+  plugins: [],
+}
+
